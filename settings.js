911492b376e524161e61bc501662021a668b0644
d33l <dgdarrell8@gmail.com>
2025-08-05 15:31:20 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;TUMFCIjb#KyY-yRV0k5F4PWzs6Eu7tYlFD_Rg1Sii7faJ09skAGo", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;nENyxKQR#qfJ59yl_NsyBR-MZWphGbGhw3bNxYbi8U81Q2FgALGI", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
