d5cc647be42ec9616bc64cc3446e8819052fac7a
d33l <dgdarrell8@gmail.com>
2025-08-05 19:18:16 +0200

    Update settings.js for subzero-

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;TUMFCIjb#KyY-yRV0k5F4PWzs6Eu7tYlFD_Rg1Sii7faJ09skAGo", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "Dee", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "26371", // Put Owner Number Here 
+  PREFIX: "!" // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
 // CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
