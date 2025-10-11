35b38837e0cdd1ceee1ef457fcac49ac70799e09
d33l <dgdarrell8@gmail.com>
2025-10-11 15:35:29 +0200

    Update settings.js for Ff

--- settings.js
+++ settings.js
@@ -1,7 +1,6 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;TUMFCIjb#KyY-yRV0k5F4PWzs6Eu7tYlFD_Rg1Sii7faJ09skAGo", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "Ff",
+  OWNER_NUMBER: "Gg", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
